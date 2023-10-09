4c7d5ee49d13121f9d826f254acf36aa22a3a28d
Mati Gummá <maalguna@gmail.com>
2023-10-09 14:01:12 -0300

    base

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 import os
-from flask import Flask, send_from_directory, render_template, redirect
+from flask import Flask, send_from_directory, render_template, redirect, request, jsonify
+import stream_chat
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -16,6 +20,34 @@ def home():
 @app.route('/<path:path>')
 def all_routes(path):
     return redirect('/')
+
+@app.route('/getstream/token', methods=['POST'])
+def get_stream_token():
+    try:
+        data = request.get_json()
+
+        if 'user_id' not in data or not data['user_id']:
+            return {"error": "Missing user_id"}
+
+        server_client = stream_chat.StreamChat(os.environ.get("app_id"), os.environ.get("stream_secret"))
+        token = server_client.create_token(data['user_id'])
+
+        return {"token": token}
+    except Exception:
+        return None;
+
+@app.route('/api/me', methods=['GET'])
+def get_user():
+    try: 
+        return {
+            "id": "Matias_7c7edd68-9a31-45e5-904f-4f9d84d0bf0b",
+            "name": "Matias",
+            "created_at": "",
+            "updated_at": ""
+        }
+    except Exception:
+        return None;
+
 
 if __name__ == "__main__":
     app.run(port=port)
